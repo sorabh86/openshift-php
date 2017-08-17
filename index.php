e36915c6f68063cec23af58fa0a5ec60a1a44c50
Sorabh <ssorabh.ssharma@gmail.com>
2017-08-17 16:58:32 +0530

    update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-Welcome to my awesome site <?php echo 'openshift-php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1>PHP Website</h1>
+  <?php
+
+    echo "Awesome site";
+
+  ?>
+</body>
+</html>
