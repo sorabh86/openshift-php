b9ecd85d1385fd04b5c364ecf729e761e18200ae
Sorabh <ssorabh.ssharma@gmail.com>
2017-09-07 14:45:23 +0530

    correct logo

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"><a href="//expertcodedesign.com/"><img alt="ExperCodeDesign Logo" src="img/logo.png"> </a></a>
+          <a class="navbar-brand" href="#"><a href="//expertcodedesign.com/"><img alt="ExperCodeDesign Logo" src="//expertcodedesign.com/img/logo.png"> </a></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
